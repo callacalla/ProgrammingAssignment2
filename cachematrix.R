1c622b1a7493f8a1f0d67be18e0140529e53648c
callacalla <rongliu0@gmail.com>
2017-01-05 21:52:38 -0600

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## make a list of functions to set and get the value of a matrix, 
+## and set and get the vaule of the inverse of the matrix from parent enviroment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL  ## initialize s (the inverse of a matrix)
+        set <- function(y) {    
+                x <<- y       ## assign the input argument to x (a matrix here )in parent environment
+                s <<- NULL    ## assign null to s in parent environment
+        }
+        get <- function() x   ## retrieve the value of x from parent environment
+        setinverse <- function(solve) s <<- solve  ## assign the input argument to s in parent environment
+        getinverse <- function() s ## retrieve the value of s from parent environment
+        list(set = set, get = get,   ## assign the four function as an element of a list, and returns it to the parent environment
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## get the inverse of a matrix from cached data or calucate the inverse and cache it
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {  ## x is the list of functions returned from makeCacheMatrix.R
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getinverse() ## retrieve the value of s from parent environment
+        if(!is.null(s)) {  ## if s exists, get this data
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()  ##x if s does not exist, retrieve the value of x (a matrix) from parent environment
+        s <- solve(data) ## calculate the inverse of the matrix 
+        x$setinverse(s) ## assign the inverse of the matrix to s in parent environment
+        s ## return s, the inverse of the matrix 
 }
